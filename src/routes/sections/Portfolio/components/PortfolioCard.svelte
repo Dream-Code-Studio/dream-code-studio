e7ec4b01bb9220ad2316db76b91692fd6e491999
Valerian Coelho <valerian27012004@gmail.com>
2024-03-18 23:58:57 +0530

    Hover Effect Added to Card

--- src/routes/sections/Portfolio/components/PortfolioCard.svelte
+++ src/routes/sections/Portfolio/components/PortfolioCard.svelte
@@ -4,21 +4,41 @@
   export let src;
 </script>
 
-<div class="card" style:background-image={`url(${src})`} style:background-size={'cover'}>
+<div class="card">
   <div class="title">{title}</div>
   <div class="info">
     {#each highlight as item}
       <div class="item">{item}</div>
     {/each}
   </div>
+  <div
+    class="img"
+    style:background-image={`url(${src})`}
+    style:background-size={"cover"}
+  ></div>
 </div>
 
 <style>
   .card {
+    position: relative;
     border: 2px solid #514955;
     display: inline-block;
     padding: 48px;
     border-radius: 12px;
+    overflow: hidden;
+  }
+  .img {
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: -1;
+    opacity: 0;
+    transition: opacity 0.3s ease-in-out;
+  }
+  .card:hover .img{
+    opacity: 1;
   }
   .title {
     font-family: "Satoshi-black";
@@ -27,14 +47,18 @@
     color: black;
     margin-bottom: 72px;
     text-shadow:
-      -1px -1px 0 #F6E3FF,
-      1px -1px 0 #F6E3FF,
-      -1px 1px 0 #F6E3FF,
-      1px 1px 0 #F6E3FF;
+      -1px -1px 0 #f6e3ff,
+      1px -1px 0 #f6e3ff,
+      -1px 1px 0 #f6e3ff,
+      1px 1px 0 #f6e3ff;
+  }
+  .card:hover .title {
+    color: #f6e3ff;
+    text-shadow: none;
   }
   .info {
-    font-family: 'Satoshi-Regular';
+    font-family: "Satoshi-Regular";
     font-size: 20px;
-    color: #F6E3FF;
+    color: #f6e3ff;
   }
 </style>
