b10aa6810d83028ff30ddd4bbbe11914d517f040
Valerian Coelho <valerian27012004@gmail.com>
2024-03-18 12:09:42 +0530

    navbar ui complete

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -13,6 +13,7 @@
     {#each navbar as item, index}
       <a class="link" on:click={() => handleClick(index)}>{item}</a>
     {/each}
+    <div class="light"></div>
     <div class="active-link">Home</div>
   </div>
 </nav>
@@ -27,6 +28,7 @@
     background-color: rgb(6, 1, 28, .7);
     border-radius: 100px;
     border: 1px solid rgb(66, 0, 97.7);
+    margin-top: 20px;
   }
   .links {
     display: inline-flex;
@@ -42,7 +44,7 @@
     justify-content: center;
     align-items: center;
     font-family: sans-serif;
-    color: white;
+    color: #F7E6FF;
     z-index: 2;
     cursor: default;
   }
@@ -60,6 +62,18 @@
     font-family: sans-serif;
     transition: 0.6s all ease-in-out;
     color: transparent;
-    transform: translate(calc(var(--index) * (100% + 12px)), 0);
+    transform: translate(calc(var(--index) * (100% + var(--gap) * 1px)), 0);
+  }
+  .light {
+    background-color: white;
+    width: 80px;
+    margin: 0 12px;
+    height: 1px;
+    position: absolute;
+    top: calc(-13px - 1px);
+    transition: 0.6s all ease-in-out;
+    transform: translate(calc(var(--index) * (100% + 24px + var(--gap) * 1px)), 0);
+    left: 0;
+    box-shadow: 0 2px 12px 0 white;
   }
 </style>
