80e3ea5f9e9419ba4c3b711314ec2630e67bef8e
Valerian Coelho <valerian27012004@gmail.com>
2024-03-20 21:35:12 +0530

    Background Svg Updated

--- src/routes/sections/Home/Background.svelte
+++ src/routes/sections/Home/Background.svelte
@@ -10,7 +10,7 @@
     y1="2.18557e-08"
     x2="102.5"
     y2="650"
-    stroke="url(#paint0_radial_8_397)"
+    stroke="url(#paint0_radial_23_599)"
     stroke-opacity="0.35"
   />
   <line
@@ -18,7 +18,7 @@
     y1="2.18557e-08"
     x2="182.5"
     y2="650"
-    stroke="url(#paint1_radial_8_397)"
+    stroke="url(#paint1_radial_23_599)"
     stroke-opacity="0.35"
   />
   <line
@@ -26,7 +26,7 @@
     y1="2.18557e-08"
     x2="262.5"
     y2="650"
-    stroke="url(#paint2_radial_8_397)"
+    stroke="url(#paint2_radial_23_599)"
     stroke-opacity="0.35"
   />
   <line
@@ -34,7 +34,7 @@
     y1="2.18557e-08"
     x2="342.5"
     y2="650"
-    stroke="url(#paint3_radial_8_397)"
+    stroke="url(#paint3_radial_23_599)"
     stroke-opacity="0.35"
   />
   <line
@@ -42,7 +42,7 @@
     y1="2.18557e-08"
     x2="422.5"
     y2="650"
-    stroke="url(#paint4_radial_8_397)"
+    stroke="url(#paint4_radial_23_599)"
     stroke-opacity="0.35"
   />
   <line
@@ -50,7 +50,7 @@
     y1="2.18557e-08"
     x2="502.5"
     y2="650"
-    stroke="url(#paint5_radial_8_397)"
+    stroke="url(#paint5_radial_23_599)"
     stroke-opacity="0.35"
   />
   <line
@@ -58,7 +58,7 @@
     y1="2.18557e-08"
     x2="582.5"
     y2="650"
-    stroke="url(#paint6_radial_8_397)"
+    stroke="url(#paint6_radial_23_599)"
     stroke-opacity="0.35"
   />
   <line
@@ -66,7 +66,7 @@
     y1="2.18557e-08"
     x2="662.5"
     y2="650"
-    stroke="url(#paint7_radial_8_397)"
+    stroke="url(#paint7_radial_23_599)"
     stroke-opacity="0.35"
   />
   <line
@@ -74,7 +74,7 @@
     y1="2.18557e-08"
     x2="742.5"
     y2="650"
-    stroke="url(#paint8_radial_8_397)"
+    stroke="url(#paint8_radial_23_599)"
     stroke-opacity="0.35"
   />
   <line
@@ -82,7 +82,7 @@
     y1="2.18557e-08"
     x2="822.5"
     y2="650"
-    stroke="url(#paint9_radial_8_397)"
+    stroke="url(#paint9_radial_23_599)"
     stroke-opacity="0.35"
   />
   <line
@@ -90,7 +90,7 @@
     y1="2.18557e-08"
     x2="902.5"
     y2="650"
-    stroke="url(#paint10_radial_8_397)"
+    stroke="url(#paint10_radial_23_599)"
     stroke-opacity="0.35"
   />
   <line
@@ -98,7 +98,7 @@
     y1="2.18557e-08"
     x2="982.5"
     y2="650"
-    stroke="url(#paint11_radial_8_397)"
+    stroke="url(#paint11_radial_23_599)"
     stroke-opacity="0.35"
   />
   <line
@@ -106,7 +106,7 @@
     y1="2.18557e-08"
     x2="1062.5"
     y2="650"
-    stroke="url(#paint12_radial_8_397)"
+    stroke="url(#paint12_radial_23_599)"
     stroke-opacity="0.35"
   />
   <line
@@ -114,7 +114,7 @@
     y1="649.5"
     x2="1163"
     y2="649.5"
-    stroke="url(#paint13_radial_8_397)"
+    stroke="url(#paint13_radial_23_599)"
     stroke-opacity="0.1"
   />
   <line
@@ -122,7 +122,7 @@
     y1="569.5"
     x2="1163"
     y2="569.5"
-    stroke="url(#paint14_radial_8_397)"
+    stroke="url(#paint14_radial_23_599)"
     stroke-opacity="0.35"
   />
   <line
@@ -130,7 +130,7 @@
     y1="489.5"
     x2="1163"
     y2="489.5"
-    stroke="url(#paint15_radial_8_397)"
+    stroke="url(#paint15_radial_23_599)"
     stroke-opacity="0.35"
   />
   <line
@@ -138,7 +138,7 @@
     y1="409.5"
     x2="1163"
     y2="409.5"
-    stroke="url(#paint16_radial_8_397)"
+    stroke="url(#paint16_radial_23_599)"
     stroke-opacity="0.35"
   />
   <line
@@ -146,7 +146,7 @@
     y1="329.5"
     x2="1163"
     y2="329.5"
-    stroke="url(#paint17_radial_8_397)"
+    stroke="url(#paint17_radial_23_599)"
     stroke-opacity="0.35"
   />
   <line
@@ -154,7 +154,7 @@
     y1="249.5"
     x2="1163"
     y2="249.5"
-    stroke="url(#paint18_radial_8_397)"
+    stroke="url(#paint18_radial_23_599)"
     stroke-opacity="0.35"
   />
   <line
@@ -162,7 +162,7 @@
     y1="169.5"
     x2="1163"
     y2="169.5"
-    stroke="url(#paint19_radial_8_397)"
+    stroke="url(#paint19_radial_23_599)"
     stroke-opacity="0.35"
   />
   <line
@@ -170,7 +170,7 @@
     y1="89.5"
     x2="1163"
     y2="89.5"
-    stroke="url(#paint20_radial_8_397)"
+    stroke="url(#paint20_radial_23_599)"
     stroke-opacity="0.35"
   />
   <line
@@ -178,12 +178,47 @@
     y1="9.5"
     x2="1163"
     y2="9.50003"
-    stroke="url(#paint21_radial_8_397)"
+    stroke="url(#paint21_radial_23_599)"
     stroke-opacity="0.1"
+  />
+  <path
+    d="M100.534 134.706C101.11 132.818 103.782 132.818 104.359 134.706L111.498 158.067C111.692 158.703 112.19 159.201 112.826 159.395L136.187 166.534C138.074 167.11 138.074 169.782 136.187 170.359L112.826 177.498C112.19 177.692 111.692 178.19 111.498 178.826L104.359 202.187C103.782 204.074 101.11 204.074 100.534 202.187L93.3951 178.826C93.2007 178.19 92.7029 177.692 92.0668 177.498L68.7057 170.359C66.8183 169.782 66.8183 167.11 68.7057 166.534L92.0668 159.395C92.7029 159.201 93.2007 158.703 93.3951 158.067L100.534 134.706Z"
+    fill="#312A35"
+  />
+  <path
+    d="M1060.09 456.259C1060.66 454.372 1063.34 454.372 1063.91 456.259L1071.05 479.621C1071.25 480.257 1071.74 480.754 1072.38 480.949L1095.74 488.087C1097.63 488.664 1097.63 491.336 1095.74 491.913L1072.38 499.051C1071.74 499.246 1071.25 499.743 1071.05 500.379L1063.91 523.741C1063.34 525.628 1060.66 525.628 1060.09 523.741L1052.95 500.379C1052.75 499.743 1052.26 499.246 1051.62 499.051L1028.26 491.913C1026.37 491.336 1026.37 488.664 1028.26 488.087L1051.62 480.949C1052.26 480.754 1052.75 480.257 1052.95 479.621L1060.09 456.259Z"
+    fill="#312A35"
+  />
+  <path
+    d="M61.001 490C61.001 467.909 78.9095 450 101.001 450C123.092 450 141.001 467.909 141.001 490C141.001 512.091 123.092 530 101.001 530C78.9095 530 61.001 512.091 61.001 490Z"
+    fill="#030009"
+    stroke="#312A35"
+    stroke-width="3"
+    stroke-miterlimit="10"
+  />
+  <path
+    d="M98.6719 477.225C98.6719 482.856 94.1074 487.42 88.477 487.42C82.8462 487.42 78.2817 482.856 78.2817 477.225C78.2817 471.595 82.8462 467.03 88.477 467.03C94.1074 467.03 98.6719 471.595 98.6719 477.225Z"
+    fill="#312A35"
+  />
+  <path
+    d="M82.7791 499.115C82.7791 501.433 80.8996 503.313 78.5812 503.313C76.2626 503.313 74.3833 501.433 74.3833 499.115C74.3833 496.796 76.2626 494.917 78.5812 494.917C80.8996 494.917 82.7791 496.796 82.7791 499.115Z"
+    fill="#312A35"
+  />
+  <path
+    d="M108.267 521.754C108.267 523.161 107.126 524.302 105.719 524.302C104.311 524.302 103.17 523.161 103.17 521.754C103.17 520.346 104.311 519.205 105.719 519.205C107.126 519.205 108.267 520.346 108.267 521.754Z"
+    fill="#312A35"
+  />
+  <path
+    d="M103.769 499.415C103.769 500.574 102.628 501.514 101.221 501.514C99.813 501.514 98.6719 500.574 98.6719 499.415C98.6719 498.255 99.813 497.316 101.221 497.316C102.628 497.316 103.769 498.255 103.769 499.415Z"
+    fill="#312A35"
+  />
+  <path
+    d="M112.165 491.319C112.165 492.644 111.091 493.717 109.767 493.717C108.442 493.717 107.368 492.644 107.368 491.319C107.368 489.994 108.442 488.92 109.767 488.92C111.091 488.92 112.165 489.994 112.165 491.319Z"
+    fill="#312A35"
   />
   <defs>
     <radialGradient
-      id="paint0_radial_8_397"
+      id="paint0_radial_23_599"
       cx="0"
       cy="0"
       r="1"
@@ -194,7 +229,7 @@
       <stop offset="1" stop-color="#EEC9FF" stop-opacity="0" />
     </radialGradient>
     <radialGradient
-      id="paint1_radial_8_397"
+      id="paint1_radial_23_599"
       cx="0"
       cy="0"
       r="1"
@@ -205,7 +240,7 @@
       <stop offset="1" stop-color="#EEC9FF" stop-opacity="0" />
     </radialGradient>
     <radialGradient
-      id="paint2_radial_8_397"
+      id="paint2_radial_23_599"
       cx="0"
       cy="0"
       r="1"
@@ -216,7 +251,7 @@
       <stop offset="1" stop-color="#EEC9FF" stop-opacity="0" />
     </radialGradient>
     <radialGradient
-      id="paint3_radial_8_397"
+      id="paint3_radial_23_599"
       cx="0"
       cy="0"
       r="1"
@@ -227,7 +262,7 @@
       <stop offset="1" stop-color="#EEC9FF" stop-opacity="0" />
     </radialGradient>
     <radialGradient
-      id="paint4_radial_8_397"
+      id="paint4_radial_23_599"
       cx="0"
       cy="0"
       r="1"
@@ -238,7 +273,7 @@
       <stop offset="1" stop-color="#EEC9FF" stop-opacity="0" />
     </radialGradient>
     <radialGradient
-      id="paint5_radial_8_397"
+      id="paint5_radial_23_599"
       cx="0"
       cy="0"
       r="1"
@@ -249,7 +284,7 @@
       <stop offset="1" stop-color="#EEC9FF" stop-opacity="0" />
     </radialGradient>
     <radialGradient
-      id="paint6_radial_8_397"
+      id="paint6_radial_23_599"
       cx="0"
       cy="0"
       r="1"
@@ -260,7 +295,7 @@
       <stop offset="1" stop-color="#EEC9FF" stop-opacity="0" />
     </radialGradient>
     <radialGradient
-      id="paint7_radial_8_397"
+      id="paint7_radial_23_599"
       cx="0"
       cy="0"
       r="1"
@@ -271,7 +306,7 @@
       <stop offset="1" stop-color="#EEC9FF" stop-opacity="0" />
     </radialGradient>
     <radialGradient
-      id="paint8_radial_8_397"
+      id="paint8_radial_23_599"
       cx="0"
       cy="0"
       r="1"
@@ -282,7 +317,7 @@
       <stop offset="1" stop-color="#EEC9FF" stop-opacity="0" />
     </radialGradient>
     <radialGradient
-      id="paint9_radial_8_397"
+      id="paint9_radial_23_599"
       cx="0"
       cy="0"
       r="1"
@@ -293,7 +328,7 @@
       <stop offset="1" stop-color="#EEC9FF" stop-opacity="0" />
     </radialGradient>
     <radialGradient
-      id="paint10_radial_8_397"
+      id="paint10_radial_23_599"
       cx="0"
       cy="0"
       r="1"
@@ -304,7 +339,7 @@
       <stop offset="1" stop-color="#EEC9FF" stop-opacity="0" />
     </radialGradient>
     <radialGradient
-      id="paint11_radial_8_397"
+      id="paint11_radial_23_599"
       cx="0"
       cy="0"
       r="1"
@@ -315,7 +350,7 @@
       <stop offset="1" stop-color="#EEC9FF" stop-opacity="0" />
     </radialGradient>
     <radialGradient
-      id="paint12_radial_8_397"
+      id="paint12_radial_23_599"
       cx="0"
       cy="0"
       r="1"
@@ -326,7 +361,7 @@
       <stop offset="1" stop-color="#EEC9FF" stop-opacity="0" />
     </radialGradient>
     <radialGradient
-      id="paint13_radial_8_397"
+      id="paint13_radial_23_599"
       cx="0"
       cy="0"
       r="1"
@@ -337,7 +372,7 @@
       <stop offset="1" stop-color="#EEC9FF" stop-opacity="0" />
     </radialGradient>
     <radialGradient
-      id="paint14_radial_8_397"
+      id="paint14_radial_23_599"
       cx="0"
       cy="0"
       r="1"
@@ -348,7 +383,7 @@
       <stop offset="1" stop-color="#EEC9FF" stop-opacity="0" />
     </radialGradient>
     <radialGradient
-      id="paint15_radial_8_397"
+      id="paint15_radial_23_599"
       cx="0"
       cy="0"
       r="1"
@@ -359,7 +394,7 @@
       <stop offset="1" stop-color="#EEC9FF" stop-opacity="0" />
     </radialGradient>
     <radialGradient
-      id="paint16_radial_8_397"
+      id="paint16_radial_23_599"
       cx="0"
       cy="0"
       r="1"
@@ -370,7 +405,7 @@
       <stop offset="1" stop-color="#EEC9FF" stop-opacity="0" />
     </radialGradient>
     <radialGradient
-      id="paint17_radial_8_397"
+      id="paint17_radial_23_599"
       cx="0"
       cy="0"
       r="1"
@@ -381,7 +416,7 @@
       <stop offset="1" stop-color="#EEC9FF" stop-opacity="0" />
     </radialGradient>
     <radialGradient
-      id="paint18_radial_8_397"
+      id="paint18_radial_23_599"
       cx="0"
       cy="0"
       r="1"
@@ -392,7 +427,7 @@
       <stop offset="1" stop-color="#EEC9FF" stop-opacity="0" />
     </radialGradient>
     <radialGradient
-      id="paint19_radial_8_397"
+      id="paint19_radial_23_599"
       cx="0"
       cy="0"
       r="1"
@@ -403,7 +438,7 @@
       <stop offset="1" stop-color="#EEC9FF" stop-opacity="0" />
     </radialGradient>
     <radialGradient
-      id="paint20_radial_8_397"
+      id="paint20_radial_23_599"
       cx="0"
       cy="0"
       r="1"
@@ -414,7 +449,7 @@
       <stop offset="1" stop-color="#EEC9FF" stop-opacity="0" />
     </radialGradient>
     <radialGradient
-      id="paint21_radial_8_397"
+      id="paint21_radial_23_599"
       cx="0"
       cy="0"
       r="1"
